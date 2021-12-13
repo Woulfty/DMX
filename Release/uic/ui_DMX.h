225df3e53e0af332279290935669e8d367677e70
Woulfty <70692837+Woulfty@users.noreply.github.com>
2021-12-13 17:16:23 +0100

    Add files via upload

---
+++ Release/uic/ui_DMX.h
@@ -0,0 +1,256 @@
+/********************************************************************************
+** Form generated from reading UI file 'DMX.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DMX_H
+#define UI_DMX_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_DMXClass
+{
+public:
+    QWidget *centralWidget;
+    QLabel *state;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QSlider *Slider12;
+    QLabel *Label12;
+    QSlider *Slider11;
+    QLabel *Label11;
+    QSlider *Slider10;
+    QSlider *Slider9;
+    QLabel *Label10;
+    QLabel *Label9;
+    QSlider *Slider7;
+    QLabel *Label8;
+    QLabel *Label7;
+    QSlider *Slider8;
+    QSlider *Slider6;
+    QLabel *Label6;
+    QSlider *Slider5;
+    QLabel *Label5;
+    QSlider *Slider3;
+    QSlider *Slider2;
+    QSlider *Slider1;
+    QSlider *Slider4;
+    QLabel *Label4;
+    QLabel *Label3;
+    QLabel *Label2;
+    QLabel *Label1;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *DMXClass)
+    {
+        if (DMXClass->objectName().isEmpty())
+            DMXClass->setObjectName(QString::fromUtf8("DMXClass"));
+        DMXClass->resize(732, 531);
+        centralWidget = new QWidget(DMXClass);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        state = new QLabel(centralWidget);
+        state->setObjectName(QString::fromUtf8("state"));
+        state->setGeometry(QRect(710, 460, 47, 13));
+        pushButton_4 = new QPushButton(centralWidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(300, 330, 171, 111));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(60, 290, 171, 23));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(300, 290, 171, 23));
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(530, 290, 171, 23));
+        Slider12 = new QSlider(centralWidget);
+        Slider12->setObjectName(QString::fromUtf8("Slider12"));
+        Slider12->setGeometry(QRect(680, 90, 19, 160));
+        Slider12->setMaximum(255);
+        Slider12->setOrientation(Qt::Vertical);
+        Label12 = new QLabel(centralWidget);
+        Label12->setObjectName(QString::fromUtf8("Label12"));
+        Label12->setGeometry(QRect(670, 260, 40, 16));
+        Label12->setAlignment(Qt::AlignCenter);
+        Slider11 = new QSlider(centralWidget);
+        Slider11->setObjectName(QString::fromUtf8("Slider11"));
+        Slider11->setGeometry(QRect(630, 90, 19, 160));
+        Slider11->setMaximum(255);
+        Slider11->setOrientation(Qt::Vertical);
+        Label11 = new QLabel(centralWidget);
+        Label11->setObjectName(QString::fromUtf8("Label11"));
+        Label11->setGeometry(QRect(620, 260, 31, 16));
+        Label11->setAlignment(Qt::AlignCenter);
+        Slider10 = new QSlider(centralWidget);
+        Slider10->setObjectName(QString::fromUtf8("Slider10"));
+        Slider10->setGeometry(QRect(580, 90, 19, 160));
+        Slider10->setMaximum(255);
+        Slider10->setOrientation(Qt::Vertical);
+        Slider9 = new QSlider(centralWidget);
+        Slider9->setObjectName(QString::fromUtf8("Slider9"));
+        Slider9->setGeometry(QRect(530, 90, 19, 160));
+        Slider9->setMaximum(255);
+        Slider9->setOrientation(Qt::Vertical);
+        Label10 = new QLabel(centralWidget);
+        Label10->setObjectName(QString::fromUtf8("Label10"));
+        Label10->setGeometry(QRect(570, 260, 31, 16));
+        Label10->setAlignment(Qt::AlignCenter);
+        Label9 = new QLabel(centralWidget);
+        Label9->setObjectName(QString::fromUtf8("Label9"));
+        Label9->setGeometry(QRect(520, 260, 31, 16));
+        Label9->setAlignment(Qt::AlignCenter);
+        Slider7 = new QSlider(centralWidget);
+        Slider7->setObjectName(QString::fromUtf8("Slider7"));
+        Slider7->setGeometry(QRect(400, 90, 19, 160));
+        Slider7->setMaximum(255);
+        Slider7->setOrientation(Qt::Vertical);
+        Label8 = new QLabel(centralWidget);
+        Label8->setObjectName(QString::fromUtf8("Label8"));
+        Label8->setGeometry(QRect(440, 260, 31, 16));
+        Label8->setAlignment(Qt::AlignCenter);
+        Label7 = new QLabel(centralWidget);
+        Label7->setObjectName(QString::fromUtf8("Label7"));
+        Label7->setGeometry(QRect(390, 260, 31, 16));
+        Label7->setAlignment(Qt::AlignCenter);
+        Slider8 = new QSlider(centralWidget);
+        Slider8->setObjectName(QString::fromUtf8("Slider8"));
+        Slider8->setGeometry(QRect(450, 90, 19, 160));
+        Slider8->setMaximum(255);
+        Slider8->setOrientation(Qt::Vertical);
+        Slider6 = new QSlider(centralWidget);
+        Slider6->setObjectName(QString::fromUtf8("Slider6"));
+        Slider6->setGeometry(QRect(350, 90, 19, 160));
+        Slider6->setMaximum(255);
+        Slider6->setOrientation(Qt::Vertical);
+        Label6 = new QLabel(centralWidget);
+        Label6->setObjectName(QString::fromUtf8("Label6"));
+        Label6->setGeometry(QRect(340, 260, 31, 16));
+        Label6->setAlignment(Qt::AlignCenter);
+        Slider5 = new QSlider(centralWidget);
+        Slider5->setObjectName(QString::fromUtf8("Slider5"));
+        Slider5->setGeometry(QRect(300, 90, 19, 160));
+        Slider5->setMaximum(255);
+        Slider5->setOrientation(Qt::Vertical);
+        Label5 = new QLabel(centralWidget);
+        Label5->setObjectName(QString::fromUtf8("Label5"));
+        Label5->setGeometry(QRect(290, 260, 31, 16));
+        Label5->setAlignment(Qt::AlignCenter);
+        Slider3 = new QSlider(centralWidget);
+        Slider3->setObjectName(QString::fromUtf8("Slider3"));
+        Slider3->setGeometry(QRect(160, 90, 19, 160));
+        Slider3->setMaximum(255);
+        Slider3->setOrientation(Qt::Vertical);
+        Slider2 = new QSlider(centralWidget);
+        Slider2->setObjectName(QString::fromUtf8("Slider2"));
+        Slider2->setGeometry(QRect(110, 90, 19, 160));
+        Slider2->setMaximum(255);
+        Slider2->setOrientation(Qt::Vertical);
+        Slider1 = new QSlider(centralWidget);
+        Slider1->setObjectName(QString::fromUtf8("Slider1"));
+        Slider1->setGeometry(QRect(60, 90, 19, 160));
+        Slider1->setMaximum(255);
+        Slider1->setOrientation(Qt::Vertical);
+        Slider1->setTickPosition(QSlider::NoTicks);
+        Slider4 = new QSlider(centralWidget);
+        Slider4->setObjectName(QString::fromUtf8("Slider4"));
+        Slider4->setGeometry(QRect(210, 90, 19, 160));
+        Slider4->setMaximum(255);
+        Slider4->setOrientation(Qt::Vertical);
+        Label4 = new QLabel(centralWidget);
+        Label4->setObjectName(QString::fromUtf8("Label4"));
+        Label4->setGeometry(QRect(200, 260, 31, 16));
+        Label4->setAlignment(Qt::AlignCenter);
+        Label3 = new QLabel(centralWidget);
+        Label3->setObjectName(QString::fromUtf8("Label3"));
+        Label3->setGeometry(QRect(150, 260, 31, 16));
+        Label3->setAlignment(Qt::AlignCenter);
+        Label2 = new QLabel(centralWidget);
+        Label2->setObjectName(QString::fromUtf8("Label2"));
+        Label2->setGeometry(QRect(100, 260, 31, 16));
+        Label2->setAlignment(Qt::AlignCenter);
+        Label1 = new QLabel(centralWidget);
+        Label1->setObjectName(QString::fromUtf8("Label1"));
+        Label1->setGeometry(QRect(50, 260, 31, 16));
+        Label1->setAlignment(Qt::AlignCenter);
+        DMXClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(DMXClass);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 732, 21));
+        DMXClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(DMXClass);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        DMXClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(DMXClass);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        DMXClass->setStatusBar(statusBar);
+
+        retranslateUi(DMXClass);
+        QObject::connect(Slider1, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider2, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider3, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider4, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider5, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider6, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider7, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider8, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider9, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider10, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider11, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(Slider12, SIGNAL(valueChanged(int)), DMXClass, SLOT(Update()));
+        QObject::connect(pushButton, SIGNAL(clicked()), DMXClass, SLOT(ResetDMX1()));
+        QObject::connect(pushButton_2, SIGNAL(clicked()), DMXClass, SLOT(ResetDMX2()));
+        QObject::connect(pushButton_3, SIGNAL(clicked()), DMXClass, SLOT(ResetDMX3()));
+        QObject::connect(pushButton_4, SIGNAL(clicked()), DMXClass, SLOT(Auto()));
+
+        QMetaObject::connectSlotsByName(DMXClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *DMXClass)
+    {
+        DMXClass->setWindowTitle(QCoreApplication::translate("DMXClass", "DMX", nullptr));
+        state->setText(QCoreApplication::translate("DMXClass", "...", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("DMXClass", "Auto", nullptr));
+        pushButton->setText(QCoreApplication::translate("DMXClass", "Reset", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("DMXClass", "Reset", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("DMXClass", "Reset", nullptr));
+        Label12->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label11->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label10->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label9->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label8->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label7->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label6->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label5->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label4->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label3->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label2->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+        Label1->setText(QCoreApplication::translate("DMXClass", "0", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class DMXClass: public Ui_DMXClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DMX_H
